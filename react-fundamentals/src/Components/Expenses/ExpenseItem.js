eb9f9620b7b0b6ceab57b0b3c52cd50ec1688b08
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-07-30 23:29:24 +0500

    Implemented UseState

--- react-fundamentals/src/Components/Expenses/ExpenseItem.js
+++ react-fundamentals/src/Components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+
+  const titleClickHandler = () => {
+    setTitle('Updated!!')
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={titleClickHandler}>Click Me!</button>
     </Card>
   );
 };
