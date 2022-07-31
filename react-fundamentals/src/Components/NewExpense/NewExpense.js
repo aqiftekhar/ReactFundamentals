99f5957f001d6d44901a8eebee9db760a5c0fec4
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-07-31 19:27:40 +0500

    Added Expense Add form Hide and show feature based on editing mode.

--- react-fundamentals/src/Components/NewExpense/NewExpense.js
+++ react-fundamentals/src/Components/NewExpense/NewExpense.js
@@ -1,15 +1,29 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 const NewExpense = (props) => {
+  const [isEditing, setEditing] = useState(false);
+
   const saveExpenseHandler = (expense) => {
     const expenseData = {
       ...expense,
     };
     props.onAddExpense(expenseData);
+    setEditing(false);
+  };
+
+  const enableEditingHandler = () => {
+    setEditing(true);
+  };
+  const disableEditingHandler = () => {
+    setEditing(false);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpense={saveExpenseHandler} />
+      {!isEditing && (
+        <button onClick={enableEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && <ExpenseForm onSaveExpense={saveExpenseHandler} onCancel = {disableEditingHandler}/>}
     </div>
   );
 };
